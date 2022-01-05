e36826f8452e79743e007365276f79628d2c00ac
jcrowe91 <jcrowe9119@gmail.com>
2022-01-04 19:51:11 -0600

    Built classes

--- FactoryPatternExercise/Program.cs
+++ FactoryPatternExercise/Program.cs
@@ -6,7 +6,14 @@ namespace FactoryPatternExercise
     {
         static void Main(string[] args)
         {
-           
+        //Create an Interface named IVehicle that has a stubbed out public void Drive method
+        // Create 2 new public classes that will conform to IVehicle
+        //Example) Car, Motorcycle,  BigRig
+        //These classes must conform to IVehicle and implement the Drive() method - which will just Console.WriteLine(“Building a new Car!”)
+        //Now we will make our static VehicleFactory class
+        //It will contain a static method GetVehicle(), that will return an IVehicle based on the amount of tires it’s given as a parameter
+        //Call this functionality in the Main method
+
         }
     }
 }
