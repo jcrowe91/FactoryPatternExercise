696b30a669ba4dc272432dc2ba8cd4b00c570436
jcrowe91 <jcrowe9119@gmail.com>
2022-01-06 19:55:31 -0600

    Finished Project

---
+++ FactoryPatternExercise/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string userInput)
+        {
+            switch (userInput)
+            {
+                case "2":
+                    return new Motorcycle();                 
+                case "4":
+                    return new Truck();
+                default:
+                    return new Motorcycle();
+            }
+        }
+    }
+    
+}
+
